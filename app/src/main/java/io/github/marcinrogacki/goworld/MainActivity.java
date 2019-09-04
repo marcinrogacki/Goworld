e41752b35231f56c4a0cfbcc8cc9ca75fb7cdbfa
Marcin Rogacki <mrogacki@nexway.com>
2019-09-04 12:41:35 +0200

    added swipe lib
    modified:   app/build.gradle
    modified:   app/src/main/java/io/github/marcinrogacki/goworld/MainActivity.java
    modified:   app/src/main/res/layout/activity_main.xml

--- app/src/main/java/io/github/marcinrogacki/goworld/MainActivity.java
+++ app/src/main/java/io/github/marcinrogacki/goworld/MainActivity.java
@@ -2,12 +2,61 @@ package io.github.marcinrogacki.goworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ViewAnimator;
+import android.view.MotionEvent;
+
+import com.github.pwittchen.swipe.library.Swipe;
+import com.github.pwittchen.swipe.library.SwipeListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewAnimator viewAnimator;
+    private Swipe swipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewAnimator = (ViewAnimator)findViewById(R.id.viewanimator);
+
+        swipe = new Swipe(70, 300);
+        SwipeListener otls = createSwipeListener();
+        swipe.setListener(otls);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        swipe.dispatchTouchEvent(event);
+        return super.dispatchTouchEvent(event);
+    }
+
+    private SwipeListener createSwipeListener() {
+        return new SwipeListener() {
+            @Override public void onSwipingLeft(final MotionEvent event) {
+            }
+
+            @Override public void onSwipedLeft(final MotionEvent event) {
+              viewAnimator.showPrevious();
+            }
+
+            @Override public void onSwipingRight(final MotionEvent event) {
+            }
+
+            @Override public void onSwipedRight(final MotionEvent event) {
+              viewAnimator.showNext();
+            }
+
+            @Override public void onSwipingUp(final MotionEvent event) {
+            }
+
+            @Override public void onSwipedUp(final MotionEvent event) {
+            }
+
+            @Override public void onSwipingDown(final MotionEvent event) {
+            }
+
+            @Override public void onSwipedDown(final MotionEvent event) {
+            }
+        };
     }
 }
